0313acc90bd62f0a364c1fe17b66b6d6fa232703
kaiyuan <kai.yuan@smitherytech.com>
2025-12-08 07:39:38 +0800

    define custom label for resource option

--- admin.config.js
+++ admin.config.js
@@ -48,6 +48,37 @@ const adminOptions = {
     withMadeWithLove: false, // Disable "made with love" component
     softwareBrothers: false,
   },
+  locale: {
+    language: "en",
+    availableLanguages: ["en"],
+    translations: {
+      en: {
+        labels: {
+          // Insura Wiz
+          claims: "Claims",
+          policies: "Policies",
+          documents: "Documents",
+          conversation_sessions: "Conversations Sessions",
+          status_updates: "Status Updates",
+          // WRS Pro
+          wrs_pro_brands: "Brands",
+          wrs_pro_products: "Products",
+          wrs_pro_rewards: "Rewards",
+          wrs_pro_campaigns: "Campaigns",
+          wrs_pro_campaign_products: "Campaign Products",
+          wrs_pro_customers: "Customers",
+          wrs_pro_identity_verifications: "Identity Verifications",
+          wrs_pro_registrations: "Registrations",
+          wrs_pro_registration_serials: "Registration Serials",
+          wrs_pro_moderation_queue: "Moderation Queue",
+          wrs_pro_support_tickets: "Support Tickets",
+          wrs_pro_installers: "Installers",
+          wrs_pro_installations: "Installations",
+          wrs_pro_chat_sessions: "Chat Sessions",
+        },
+      },
+    },
+  },
   resources: [
     {
       resource: {
